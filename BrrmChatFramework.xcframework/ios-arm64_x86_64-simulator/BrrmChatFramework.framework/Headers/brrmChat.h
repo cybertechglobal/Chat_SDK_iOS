c3af9056b404447702f56c2159c5dbad635885b5
Stefan Djordjevic <baki94@hotmail.rs>
2024-04-15 14:19:32 +0200

    Initial commit framework

---
+++ BrrmChatFramework.xcframework/ios-arm64_x86_64-simulator/BrrmChatFramework.framework/Headers/brrmChat.h
@@ -0,0 +1,18 @@
+//
+//  brrmChat.h
+//  brrmChat
+//
+//  Created by Stefan Djordjevic on 10/25/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for brrmChat.
+FOUNDATION_EXPORT double brrmChatVersionNumber;
+
+//! Project version string for brrmChat.
+FOUNDATION_EXPORT const unsigned char brrmChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <brrmChat/PublicHeader.h>
+
+
